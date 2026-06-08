36334b2ed782891a7ecfcb899a03d7fc15dd13cb
Aniketpatil7278 <142803348+Aniketpatil7278@users.noreply.github.com>
2026-06-08 13:55:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import {  useEffect } from "react"
+import { getLandingData } from "./api/landingApi";
+function App() {
+
+
+
+  const fetchdata = async () => {
+    try {
+      const data = await getLandingData();
+      console.log(data);
+    } catch (e) {
+      console.log("error to fetch the data ", e);
+    }
+  };
+    useEffect(() => {
+      fetchdata();
+    }, []);
+
+  return (
+    <>
+      <h1>anipatil</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/api/landingApi.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API = "https://6a2107e4b1d0aaf32b4e9ec3.mockapi.io/landing-content";
+
+export const getLandingData = async () => {
+  const response = await axios.get(API);
+  return response.data;
+};
